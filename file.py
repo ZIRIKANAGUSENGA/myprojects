5121c7ac50672fd2309f334040e0cda8a2727c21
ZIRIKANA <zirikanagusenga@gmail.com>
2024-09-01 13:16:35 -0700

    first push

---
+++ file.py
@@ -0,0 +1,9 @@
+def summation(a, b):
+    total = a + b
+    return total
+
+a = int(input('Enter a number: '))
+b = int(input('Enter another number: '))
+
+result = summation(a, b)
+print('The sum is:', result)
